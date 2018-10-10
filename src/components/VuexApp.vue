75e17f810f57b69169c8b8996dac668eaab600b3
duanjunyan <duanjunyan@autohome.com.cn>
2018-10-10 18:08:18 +0800

    init code

---
+++ src/components/VuexApp.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		hello world!!!
+	</div>
+</template>
+
+<script>
+export default {
+
+  name: 'VuexApp',
+
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="css" scoped>
+</style>
