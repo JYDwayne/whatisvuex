d4b354dc0ac2160d0aac382328d0eee694a2a559
duanjunyan <duanjunyan@autohome.com.cn>
2018-10-11 17:58:07 +0800

    "add store"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Stroe from './store/index'
 
 Vue.config.productionTip = false
 
+
+console.log(Stroe)
+
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  Stroe
 }).$mount('#app')
